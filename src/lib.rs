1d1c1a5c0429e72febccfa495ada5bcc76559edc
andrew <>
2023-12-22 21:45:46 +0900

    Add consuming log methods to ResultContext

--- src/lib.rs
+++ src/lib.rs
@@ -53,8 +53,8 @@ impl Level {
 #[macro_export]
 macro_rules! ea{
     ($($k: ident = $v: expr), *) => {
-        | attrs | {
-            $(attrs.insert(stringify!($k), $v.to_string());) *
+        | _attrs | {
+            $(_attrs.insert(stringify!($k), $v.to_string());) *
         }
     };
 }
@@ -481,9 +481,20 @@ impl Log {
 }
 
 pub trait ErrContext {
+    /// Add a simple context string onto an error, converting it to `loga::Error` in
+    /// the process.
     fn context(self, message: &'static str) -> Error;
+
+    /// Add a simple context string and attributes pairs onto an error, converting it
+    /// to `loga::Error` in the process.
     fn context_with(self, message: &'static str, attrs: impl Fn(&mut HashMap<&'static str, String>) -> ()) -> Error;
+
+    /// Add attributes from the log as well as a simple context string to an error,
+    /// converting it to `loga::Error` in the process.
     fn log_context(self, log: &Log, message: &'static str) -> Error;
+
+    /// Add attributes from the log as well as the specified attributes and a simple
+    /// context string to an error, converting it to `loga::Error` in the process.
     fn log_context_with(
         self,
         log: &Log,
@@ -548,19 +559,39 @@ impl<T: Into<Error>> ErrContext for T {
 }
 
 pub trait ResultContext<O> {
+    /// If the value is Err/None, add a simple context string onto an error, converting
+    /// it to `loga::Error` in the process.
     fn context(self, message: &'static str) -> Result<O, Error>;
+
+    /// If the value is Err/None, add a simple context string and attributes pairs onto
+    /// an error, converting it to `loga::Error` in the process.
     fn context_with(
         self,
         message: &'static str,
         attrs: impl Fn(&mut HashMap<&'static str, String>) -> (),
     ) -> Result<O, Error>;
+
+    /// If the value is Err/None, add attributes from the log as well as a simple
+    /// context string to an error, converting it to `loga::Error` in the process.
     fn log_context(self, log: &Log, message: &'static str) -> Result<O, Error>;
+
+    /// If the value is Err/None, add attributes from the log as well as the specified
+    /// attributes and a simple context string to an error, converting it to
+    /// `loga::Error` in the process.
     fn log_context_with(
         self,
         log: &Log,
         message: &'static str,
         attrs: impl Fn(&mut HashMap<&'static str, String>) -> (),
     ) -> Result<O, Error>;
+
+    // If the value is Err/None, consume it, logging it with the additional context
+    // message.
+    fn warn(self, log: &Log, message: &'static str);
+
+    // If the value is Err/None, consume it, logging it with the additional context
+    // message and attributes.
+    fn warn_with(self, log: &Log, message: &'static str, attrs: impl Fn(&mut HashMap<&'static str, String>) -> ());
 }
 
 impl<O, T: Into<Error>> ResultContext<O> for Result<O, T> {
@@ -600,6 +631,18 @@ impl<O, T: Into<Error>> ResultContext<O> for Result<O, T> {
             Err(e) => Err(e.log_context_with(log, message, attrs)),
         }
     }
+
+    fn warn(self, log: &Log, message: &'static str) {
+        if let Err(e) = self {
+            log.warn_e(e.into(), message, ea!());
+        }
+    }
+
+    fn warn_with(self, log: &Log, message: &'static str, attrs: impl Fn(&mut HashMap<&'static str, String>) -> ()) {
+        if let Err(e) = self {
+            log.warn_e(e.into(), message, attrs);
+        }
+    }
 }
 
 impl<O> ResultContext<O> for Option<O> {
@@ -637,6 +680,18 @@ impl<O> ResultContext<O> for Option<O> {
         match self {
             Some(x) => Ok(x),
             None => Err(log.new_err_with(message, attrs)),
+        }
+    }
+
+    fn warn(self, log: &Log, message: &'static str) {
+        if self.is_none() {
+            log.warn(message, ea!());
+        }
+    }
+
+    fn warn_with(self, log: &Log, message: &'static str, attrs: impl Fn(&mut HashMap<&'static str, String>) -> ()) {
+        if self.is_none() {
+            log.warn(message, attrs);
         }
     }
 }
